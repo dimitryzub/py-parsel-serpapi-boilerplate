d5b0bfa673ed3b0476ffa3d271cbf015f737a182
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-04-29 18:03:56 +0300

    add bs4 boilerplate

---
+++ bs4_solution.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests, json, os
+
+
+def parsel_scrape():
+    # https://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls
+    params = {
+        "q": "query",          # search query
+        "hl": "en",            # language of the search
+        "gl": "us"             # country of the search
+    }
+    
+    # https://docs.python-requests.org/en/master/user/quickstart/#custom-headers
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+    }
+
+    html = requests.get("https://scholar.google.com/scholar", params=params, headers=headers, timeout=30)
+    selector = BeautifulSoup(html.text)
+    
